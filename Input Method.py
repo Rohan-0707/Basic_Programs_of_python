8e2b82bfdd025814b578f1f692cfeb0b6d1adc7c
Rohan Kumar Bhoi <88358124+Rohan-0707@users.noreply.github.com>
2022-08-08 10:39:51 +0530

    Add files via upload

---
+++ Input Method.py
@@ -0,0 +1,11 @@
+print("")
+print("")
+print("This is the the Program for Accessing the valuse or any input from user !")
+print("")
+
+# Starting of Program...
+
+a = input("Enter Your Name : ")         # Here we declare a variable 'a' to take the input using input() method of python to take the inpu from user
+print("")
+print("Your Name is : "+a)                                # Simply we print the the 'a' which taken from user.
+print("")
