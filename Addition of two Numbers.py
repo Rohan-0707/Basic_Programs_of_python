eb50530f02f52026b56fbac9e7adead6afdf3443
Rohan Kumar Bhoi <88358124+Rohan-0707@users.noreply.github.com>
2022-08-08 11:01:29 +0530

    Add files via upload

---
+++ Addition of two Numbers.py
@@ -0,0 +1,60 @@
+print("")
+print("")
+print("This is the Program for Addition of two Numbers !")
+print("")
+
+# Starting of Program..
+
+a = 10              # Here we declare variable 'a' as 10
+b = 20              # and variable 'b' as 20
+
+
+c = a + b           # we declare one more variable 'c' to store the addition of a and b.
+
+print("")
+print("Additon :")
+print("")
+print(c)
+print("")
+
+# we can able to de another arithematic operations by using this method, we just need to change the operator.
+
+# Other Arithematic Programs
+print("-------------------------------------------")
+print("")
+print("Other Arithematic Programs...")
+print("")
+print("-------------------------------------------")
+print("")
+
+# Substraction...
+
+print("Substraction :")
+print("")
+c = b - a           # Here we used '-' operator only
+print(c)
+print("")
+
+# Multicplication...
+
+print("Multiplication :")
+print("")
+c = a * b           # Here we used '*' operator only
+print(c)
+print("")
+
+# Division...
+
+print("Division :")
+print("")
+c = a / b           # Here we used '/' operator only
+print(c)
+print("")
+
+# Modulation
+
+print("Modules :")
+print("")
+c = a % b           # Here we used '%' operator only
+print(c)
+print("")
