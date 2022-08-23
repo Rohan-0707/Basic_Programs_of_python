93c4d94d796cbad89e73d59f2143b4590eb2c8c7
Rohan Kumar Bhoi <88358124+Rohan-0707@users.noreply.github.com>
2022-08-23 06:24:23 +0530

    Add files via upload

---
+++ Tables.py
@@ -0,0 +1,10 @@
+#   This is the code for Table Creation of any Number !
+#   Here we can able to create any table of any any number !
+
+a = 1                   # Here we create a variable 'a' for incremental object, which helps to multiply the number in range of 1 to 10
+b = input("Enter The Number : ")    # This is our another variable, which takes a number from user to create their table !
+for i in range(1,10+1):             # Here we used a for loop
+    b = int(b)                      # Here we can convert the string value to number which taken from user !
+    c = b * a                       # Here we create another variable 'c' for multiplying the number with incremental object
+    a += 1                          # for incrementing the value of 'a' by 1, we used this formula
+    print(c)                        # At the last we can print the last output of our table
