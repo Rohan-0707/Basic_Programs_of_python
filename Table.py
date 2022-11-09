2353b1a364daff61715aba015f7dee0e9755f3ac
Rohan Kumar Bhoi <88358124+Rohan-0707@users.noreply.github.com>
2022-11-09 22:57:20 +0530

    Create Table.py
    In this video, I was created a Python Program to Display the Table of a Number.
    In this Program after the execution, the program takes a number from user and thenit will shows the Table of those number !

---
+++ Table.py
@@ -0,0 +1,37 @@
+# <!-- This code is designed by Mr. Rohan Kumar Bhoi -->
+# <!-- GitHub : github.com/rohan-0707 -->
+# <!-- Instagram : i_am_the_rohan_ -->
+# <!-- Facebook : facebook.com/rohan.bhoi.07 -->
+# <!-- Mail ID : rohanbhoi1546@gmail.com -->
+# <!-- Linkdi : rohan-kumar-bhoi -->
+# <!-- Twitter : @rohanbhoi07 -->
+
+# Table Creation in Python !
+
+print("")
+print("")
+
+num = input("Enter the number : ") # we used variable num to take the input from the user 
+
+num = int(num) # Here we converted num to integer !
+
+print("")
+print("")
+
+print(f"Table of {num} : ")
+print("")
+print("")
+
+for i in range(1,10+1):  # Here we used  afor loop to iterate our code 10 times !
+    result = num * i # Here we used another variable rusult to store the multiplication.
+    print(f"    {num} * {i} =  {result}")
+    
+    
+print("")
+print("")
+
+
+# <!-- ____________________________________________________________________________________________________________________________________________________
+#                                                                          Thank You !
+# ____________________________________________________________________________________________________________________________________________________ -->
+
