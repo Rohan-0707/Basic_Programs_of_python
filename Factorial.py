622ed299331a3475893a5840dd9cc842eb8e2070
Rohan Kumar Bhoi <88358124+Rohan-0707@users.noreply.github.com>
2022-11-17 06:48:15 +0530

    Create Factorial.py
    In this program we written a code to calculate the factorial of any number...!

---
+++ Factorial.py
@@ -0,0 +1,30 @@
+# <!-- This code is designed by Mr. Rohan Kumar Bhoi -->
+# <!-- GitHub : github.com/rohan-0707 -->
+# <!-- Instagram : i_am_the_rohan_ -->
+# <!-- Facebook : facebook.com/rohan.bhoi.07 -->
+# <!-- Mail ID : rohanbhoi1546@gmail.com -->
+# <!-- LinkdIn : rohan-kumar-bhoi -->
+# <!-- Twitter : @rohanbhoi07 -->
+# <!-- Whatsapp : http://bit.ly/3WVFh1W -->
+
+print("")
+print("find the Factorial of an Number !")
+
+print("")
+
+num = int(input("Please enter a Number : "))  # Hare we created a variable NUM to take the input from User.
+
+result = 1                                    # This our another variable which can used to store the result of our factorial
+
+for i in range(1,num+1):                      # Here we used a for-loop to interate the variable i in between 1 to  given number.
+    result = result * i                       # Here we are actually used our variable result to store our factorial
+    
+print("")
+print(f"The factorial of {num} is {result}")  # Here we used f-string to print the values of variable in our print statement
+print("")
+
+
+# <!-- ____________________________________________________________________________________________________________________________________________________
+#                                                                          Thank You !
+# ______________________________________________________________________________________________________________________________________________________ -->
+
